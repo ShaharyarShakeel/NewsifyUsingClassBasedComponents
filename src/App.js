d992a97bbcfd02b69d40bf5a4eef2ad75272a702
Shaharyar Shakeel <shaharyarshakeel1@gmail.com>
2024-07-29 02:29:41 +0500

    Newsify using function based components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 import {
@@ -9,37 +9,31 @@ import {
 import LoadingBar from 'react-top-loading-bar'
 
 
-export default class App extends Component {
-  apiKey = process.env.REACT_APP_API_KEY;
-  state ={
-    progress: 0
+export default function App() {
+  const apiKey = "d5409d358c1b49d8bba2b4607353bbbe";
+  const [progress, setProgress] = useState(0);
+  const mySetProgress = (progress)=> {
+    setProgress(progress);
   }
-
-  setProgress = (progress)=> {
-    this.setState({progress: progress})
-  }
-
-  render() {
     return (
       <div>
         <Router>
           <Navbar />
           <LoadingBar
             color='#f11946'
-            progress={this.state.progress}
+            progress={progress}
           />
           <Routes>
-            <Route exact path="/" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='general' pageSize={15} country='in' category='general' />}></Route>
-            <Route exact path="/business" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='business' pageSize={15} country='in' category='business' />}></Route>
-            <Route exact path="/entertainment" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='entertainment' pageSize={15} country='in' category='entertainment' />}></Route>
-            <Route exact path="/general" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='general' pageSize={15} country='in' category='general' />}></Route>
-            <Route exact path="/health" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='health' pageSize={15} country='in' category='health' />}></Route>
-            <Route exact path="/science" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='science' pageSize={15} country='in' category='science' />}></Route>
-            <Route exact path="/sports" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='sports' pageSize={15} country='in' category='sports' />}></Route>
-            <Route exact path="/technology" element={<News setProgress =  {this.setProgress} apiKey = {this.apiKey} key='technology' pageSize={15} country='in' category='technology' />}></Route>
+            <Route exact path="/" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='general' pageSize={15} country='in' category='general' />}></Route>
+            <Route exact path="/business" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='business' pageSize={15} country='in' category='business' />}></Route>
+            <Route exact path="/entertainment" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='entertainment' pageSize={15} country='in' category='entertainment' />}></Route>
+            <Route exact path="/general" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='general' pageSize={15} country='in' category='general' />}></Route>
+            <Route exact path="/health" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='health' pageSize={15} country='in' category='health' />}></Route>
+            <Route exact path="/science" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='science' pageSize={15} country='in' category='science' />}></Route>
+            <Route exact path="/sports" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='sports' pageSize={15} country='in' category='sports' />}></Route>
+            <Route exact path="/technology" element={<News mySetProgress =  {mySetProgress} apiKey = {apiKey} key='technology' pageSize={15} country='in' category='technology' />}></Route>
           </Routes>
         </Router>
       </div>
     )
-  }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageURL, newsURL, author, date, source } = this.props;
-    return (
-      <div className='my-3'>
-        <div className="card">
-          <div style={{ position: 'relative' }}>
-            <span className="badge rounded-pill bg-danger" style={{display: "flex", justifyContent: 'flex-end', position: 'absolute', right: -5, top:-10}}>
-              {source}
-            </span>
-          </div>
-          <img src={imageURL} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text"><small>By {author} on {new Date(date).toGMTString()}</small></p>
-            <a rel="noreferrer" href={newsURL} target='_blank' className="btn btn-sm btn-dark">Read More</a>
-          </div>
+export default function NewsItem(props) {
+  let { title, description, imageURL, newsURL, author, date, source } = props;
+  return (
+    <div className='my-3'>
+      <div className="card">
+        <div style={{ position: 'relative' }}>
+          <span className="badge rounded-pill bg-danger" style={{ display: "flex", justifyContent: 'flex-end', position: 'absolute', right: -5, top: -10 }}>
+            {source}
+          </span>
+        </div>
+        <img src={imageURL} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text"><small>By {author} on {new Date(date).toGMTString()}</small></p>
+          <a rel="noreferrer" href={newsURL} target='_blank' className="btn btn-sm btn-dark">Read More</a>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default NewsItem
